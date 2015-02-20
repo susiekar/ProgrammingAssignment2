d76547d306b1025c001f923665490b80e86fa453
susiekar <susanne.karlsson@pointcollege.fi>
2015-02-20 14:32:14 +0200

    Added comment to describe what the function does

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Matrix inversion is usually a costly computation and there may be some benefit
+# to caching the inverse of a matrix rather than compute it repeatedly. The
+# following two functions are used to cache the inverse of a matrix.
 
 ## Write a short comment describing this function
 
